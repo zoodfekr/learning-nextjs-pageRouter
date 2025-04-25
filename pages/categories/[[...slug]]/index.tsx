b2a15cb12f298d22e1508d90ffc92638801d8057
zoodfekr <zoodfekr.r@gmail.com>
2025-04-25 12:02:54 +0330

    use tailwind and link in app

---
+++ pages/categories/[[...slug]]/index.tsx
@@ -0,0 +1,49 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+
+const Categories = () => {
+
+    const router = useRouter()
+
+    const slugValue: string | string[] = router.query.slug || []
+
+    console.log(slugValue);
+
+    const testRouter =[
+        {
+            id: 1, name: 'category 1 ', href: '1'
+        },
+        {
+            id: 2, name: 'category 2 ', href: '2'
+        },
+        {
+            id: 3, name: 'category 3 ', href: '3'
+        }
+    ]
+
+
+    if (slugValue?.length == 0) {
+
+        return (
+            <>
+                <h3>home categories</h3>
+                <div className="flex flex-col gap-3 p-3 border border-red-500">
+                    {testRouter.map(val =>
+                        <Link key={val.id} href={val.href}>{val.name}</Link>
+                    )}
+                </div>
+            </>
+        )
+    } else {
+        return (
+
+            <>
+                <div className='text1' > Categories {slugValue}</div>
+            </>
+        )
+    }
+
+}
+
+export default Categories
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}", // اطمینان حاصل کنید که مسیرها صحیح هستند
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };  
