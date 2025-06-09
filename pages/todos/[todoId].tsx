ee339e443baa5e759737de6edf3b1f2d428758ee
zoodfekr <zoodfekr.r@gmail.com>
2025-06-09 16:07:04 +0330

    resolved swr bugs

--- pages/todos/[todoId].tsx
+++ pages/todos/[todoId].tsx
@@ -1,27 +1,34 @@
-
+'use client'
 import { useParams } from 'next/navigation'
 import useSWR from 'swr'
-
+import NotFound from '@/components/common/NotFound'
 import Show_Todo from '@/components/todos/Show_Todo'
 import { TodoType } from '@/types/todos'
 
-const fetcher = async (todoId: string): Promise<TodoType> => {
-    console.log({ todoId })
-    const res = await fetch(`http://localhost:9000/todos/${todoId}`)
+const getData = async (url: string): Promise<TodoType> => {
+    const res = await fetch(url)
+    if (!res.ok) throw new Error('Failed to fetch')
     return res.json()
 }
 
-
 const TodoPage = () => {
+    const params = useParams()
+    const todoId = params?.todoId as string
 
-    const { todoId } = useParams(); // دریافت todoId از پارامترهای URL
-    const { data, error } = useSWR(todoId, fetcher);
+    const { data, isLoading, error } = useSWR(
+        todoId ? `http://localhost:9000/todos/${todoId}` : null,
+        getData
+    )
 
-    console.log('todoId >>', todoId)
+    if (isLoading || !todoId) {
+        return <div>در حال بارگذاری...</div>
+    }
 
-    if (error) return <div>Failed to load</div>;
-    if (!data) return <div>loading ... </div>;
+    if (error || !data) {
+        return <NotFound />
+    }
 
-    return <Show_Todo data={data} />;
-};
+    return <Show_Todo data={data} />
+}
+
 export default TodoPage
